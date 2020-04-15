a24e67f38be0625f361aee0527583caba47dea8c
Vladimir Ivanov <vladimir.d.ivanov@jetbrains.com>
2020-04-15 18:48:22 +0300

    [runtime] Fix snprintf usage

--- runtime/src/main/cpp/Porting.cpp
+++ runtime/src/main/cpp/Porting.cpp
@@ -110,16 +110,19 @@ void consolePrintf(const char* format, ...) {
   char buffer[1024];
   va_list args;
   va_start(args, format);
-  int rv = vsnprintf_impl(buffer, sizeof(buffer) - 1, format, args);
+  int rv = vsnprintf_impl(buffer, sizeof(buffer), format, args);
+  if (rv >= sizeof(buffer)) rv = sizeof(buffer) - 1;  // TODO: Consider realloc or report truncating.
   va_end(args);
   consoleWriteUtf8(buffer, rv);
 }
 
+// TODO: Avoid code duplication.
 void consoleErrorf(const char* format, ...) {
   char buffer[1024];
   va_list args;
   va_start(args, format);
-  int rv = vsnprintf_impl(buffer, sizeof(buffer) - 1, format, args);
+  int rv = vsnprintf_impl(buffer, sizeof(buffer), format, args);
+  if (rv >= sizeof(buffer)) rv = sizeof(buffer) - 1;  // TODO: Consider realloc or report truncating.
   va_end(args);
   consoleErrorUtf8(buffer, rv);
 }
